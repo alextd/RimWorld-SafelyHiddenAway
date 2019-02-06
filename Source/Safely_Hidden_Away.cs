5463fe212892be55b7e5892f882d8687c6a5cf58
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-02-06 11:10:26 -0800

    Check if DefOfHelper patch already exists so my other mods don't patch it needlessly

--- Source/Safely_Hidden_Away.cs
+++ Source/Safely_Hidden_Away.cs
@@ -18,8 +18,9 @@ namespace Safely_Hidden_Away
 			HarmonyInstance harmony = HarmonyInstance.Create("uuugggg.rimworld.SafelyHiddenAway.main");
 			
 			//Turn off DefOf warning since harmony patches trigger it.
-			harmony.Patch(AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor"),
-				new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
+			MethodInfo DefOfHelperInfo = AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor");
+			if (!harmony.GetPatchedMethods().Contains(DefOfHelperInfo))
+				harmony.Patch(DefOfHelperInfo, new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
 			
 			harmony.PatchAll();
 		}
